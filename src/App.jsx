aa64e15a5529ba1c78327c8941555d14a9295330
Gideon Kofi <kofi>
2025-09-19 14:02:32 +0100

    mindburst opened

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 // src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
-// import AdminHome from "../admin/adminHome";
-// import Login from "../admin/adminLogin";
-// import Signup from "../admin/adminSignup";
-// import Dashboard from "../admin/adminDashboard";
-// import CreateArticle from "../admin/adminCreateArticle";
-// import Home from "../pages/home";
-// import Profile from "../admin/adminProfile";
-// import SiteSettings from "../admin/adminSiteSettings";
-// import ProtectedRoute from "../protectors/protector";
-// import ManageJournals from "../admin/manageJournals";
-// import FeedPage from "../pages/feedPage";
-// import FeedCategory from "../pages/feedCategory";
-import HoldPage from "../pages/holdPage";
+import AdminHome from "../admin/adminHome";
+import Login from "../admin/adminLogin";
+import Signup from "../admin/adminSignup";
+import Dashboard from "../admin/adminDashboard";
+import CreateArticle from "../admin/adminCreateArticle";
+import Home from "../pages/home";
+import Profile from "../admin/adminProfile";
+import SiteSettings from "../admin/adminSiteSettings";
+import ProtectedRoute from "../protectors/protector";
+import ManageJournals from "../admin/manageJournals";
+import FeedPage from "../pages/feedPage";
+import FeedCategory from "../pages/feedCategory";
+// import HoldPage from "../pages/holdPage"; // ❌ no longer needed
 
 function App() {
   return (
     <>
       <Routes>
-        {/* All existing routes commented out */}
-        {/*
+        {/* Public routes */}
         <Route path="/" element={<Home />} />
         <Route path="/feed/:id" element={<FeedPage />} />
         <Route path="/feed/category/:category" element={<FeedCategory />} />
+
+        {/* Admin routes */}
         <Route path="/admin-home" element={<AdminHome />} />
         <Route path="/admin-login" element={<Login />} />
         <Route path="/admin-signup" element={<Signup />} />
 
+        {/* Protected routes */}
         <Route
           path="/dashboard"
           element={
@@ -68,10 +70,6 @@ function App() {
             </ProtectedRoute>
           }
         />
-        */}
-
-        {/* Replacement hold page route */}
-        <Route path="*" element={<HoldPage />} />
       </Routes>
 
       <Toaster richColors position="top-right" />
